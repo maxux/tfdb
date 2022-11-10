d04a290aac3dab2c37709202156ca29abfe63e92
Maxime Daniel <root@maxux.net>
2022-11-10 15:50:07 +0100

    core: compiles against development_38

--- tfdb.v
+++ tfdb.v
@@ -1,14 +1,14 @@
 module main
 
 import freeflowuniverse.crystallib.redisserver
-import freeflowuniverse.crystallib.resp2
+import freeflowuniverse.crystallib.resp
 
-fn command_ping(input resp2.RValue, mut _ redisserver.RedisInstance) resp2.RValue {
-	if resp2.get_redis_array_len(input) > 1 {
-		return resp2.get_redis_array(input)[1]
+fn command_ping(input resp.RValue, mut _ redisserver.RedisInstance) resp.RValue {
+	if resp.get_redis_array_len(input) > 1 {
+		return resp.get_redis_array(input)[1]
 	}
 
-	return resp2.r_string('PONG')
+	return resp.r_string('PONG')
 }
 
 fn main() {
@@ -23,6 +23,62 @@ fn main() {
 		command: 'PING'
 		handler: command_ping
 	}
+
+	h << redisserver.RedisHandler{
+		command: 'NAMEDEFINE'
+		handler: command_ping
+	}
+
+	h << redisserver.RedisHandler{
+		command: 'GROUPDEFINE'
+		handler: command_ping
+	}
+
+	h << redisserver.RedisHandler{
+		command: 'HASHDEFINE'
+		handler: command_ping
+	}
+
+	h << redisserver.RedisHandler{
+		command: 'CIRCLEGET'
+		handler: command_ping
+	}
+
+	h << redisserver.RedisHandler{
+		command: 'HASHGET'
+		handler: command_ping
+	}
+
+	h << redisserver.RedisHandler{
+		command: 'HASHDELETE'
+		handler: command_ping
+	}
+
+	h << redisserver.RedisHandler{
+		command: 'HSET'
+		handler: command_ping
+	}
+
+	h << redisserver.RedisHandler{
+		command: 'HSECURE'
+		handler: command_ping
+	}
+
+	h << redisserver.RedisHandler{
+		command: 'HSCAN'
+		handler: command_ping
+	}
+
+	h << redisserver.RedisHandler{
+		command: 'HLEN'
+		handler: command_ping
+	}
+
+	h << redisserver.RedisHandler{
+		command: 'HDEL'
+		handler: command_ping
+	}
+
 
 	for {
 		println('still looping...')
