a067aa4358329377a4050c36b8d078518c610779
Maxime Daniel <root@maxux.net>
2022-11-11 01:44:43 +0100

    core: implement dummy handlers

--- tfdb.v
+++ tfdb.v
@@ -11,6 +11,96 @@ fn command_ping(input resp.RValue, mut _ redisserver.RedisInstance) resp.RValue 
 	return resp.r_string('PONG')
 }
 
+fn command_namedefine(input resp.RValue, mut _ redisserver.RedisInstance) resp.RValue {
+	if resp.get_redis_array_len(input) > 1 {
+		return resp.get_redis_array(input)[1]
+	}
+
+	return resp.r_string('PONG')
+}
+
+fn command_groupdefine(input resp.RValue, mut _ redisserver.RedisInstance) resp.RValue {
+	if resp.get_redis_array_len(input) > 1 {
+		return resp.get_redis_array(input)[1]
+	}
+
+	return resp.r_string('PONG')
+}
+
+fn command_hashdefine(input resp.RValue, mut _ redisserver.RedisInstance) resp.RValue {
+	if resp.get_redis_array_len(input) > 1 {
+		return resp.get_redis_array(input)[1]
+	}
+
+	return resp.r_string('PONG')
+}
+
+fn command_circleget(input resp.RValue, mut _ redisserver.RedisInstance) resp.RValue {
+	if resp.get_redis_array_len(input) > 1 {
+		return resp.get_redis_array(input)[1]
+	}
+
+	return resp.r_string('PONG')
+}
+
+fn command_hashget(input resp.RValue, mut _ redisserver.RedisInstance) resp.RValue {
+	if resp.get_redis_array_len(input) > 1 {
+		return resp.get_redis_array(input)[1]
+	}
+
+	return resp.r_string('PONG')
+}
+
+fn command_hashdelete(input resp.RValue, mut _ redisserver.RedisInstance) resp.RValue {
+	if resp.get_redis_array_len(input) > 1 {
+		return resp.get_redis_array(input)[1]
+	}
+
+	return resp.r_string('PONG')
+}
+
+fn command_hset(input resp.RValue, mut _ redisserver.RedisInstance) resp.RValue {
+	if resp.get_redis_array_len(input) > 1 {
+		return resp.get_redis_array(input)[1]
+	}
+
+	return resp.r_string('PONG')
+}
+
+fn command_hsecure(input resp.RValue, mut _ redisserver.RedisInstance) resp.RValue {
+	if resp.get_redis_array_len(input) > 1 {
+		return resp.get_redis_array(input)[1]
+	}
+
+	return resp.r_string('PONG')
+}
+
+fn command_hscan(input resp.RValue, mut _ redisserver.RedisInstance) resp.RValue {
+	if resp.get_redis_array_len(input) > 1 {
+		return resp.get_redis_array(input)[1]
+	}
+
+	return resp.r_string('PONG')
+}
+
+fn command_hlen(input resp.RValue, mut _ redisserver.RedisInstance) resp.RValue {
+	if resp.get_redis_array_len(input) > 1 {
+		return resp.get_redis_array(input)[1]
+	}
+
+	return resp.r_string('PONG')
+}
+
+fn command_hdel(input resp.RValue, mut _ redisserver.RedisInstance) resp.RValue {
+	if resp.get_redis_array_len(input) > 1 {
+		return resp.get_redis_array(input)[1]
+	}
+
+	return resp.r_string('PONG')
+}
+
+
+
 fn main() {
 	mut srv := redisserver.listen('0.0.0.0', 5555) or {
 		panic("Can't Listen on port with error: $err")
@@ -26,57 +116,57 @@ fn main() {
 
 	h << redisserver.RedisHandler{
 		command: 'NAMEDEFINE'
-		handler: command_ping
+		handler: command_namedefine
 	}
 
 	h << redisserver.RedisHandler{
 		command: 'GROUPDEFINE'
-		handler: command_ping
+		handler: command_groupdefine
 	}
 
 	h << redisserver.RedisHandler{
 		command: 'HASHDEFINE'
-		handler: command_ping
+		handler: command_hashdefine
 	}
 
 	h << redisserver.RedisHandler{
 		command: 'CIRCLEGET'
-		handler: command_ping
+		handler: command_circleget
 	}
 
 	h << redisserver.RedisHandler{
 		command: 'HASHGET'
-		handler: command_ping
+		handler: command_hashget
 	}
 
 	h << redisserver.RedisHandler{
 		command: 'HASHDELETE'
-		handler: command_ping
+		handler: command_hashdelete
 	}
 
 	h << redisserver.RedisHandler{
 		command: 'HSET'
-		handler: command_ping
+		handler: command_hset
 	}
 
 	h << redisserver.RedisHandler{
 		command: 'HSECURE'
-		handler: command_ping
+		handler: command_hsecure
 	}
 
 	h << redisserver.RedisHandler{
 		command: 'HSCAN'
-		handler: command_ping
+		handler: command_hscan
 	}
 
 	h << redisserver.RedisHandler{
 		command: 'HLEN'
-		handler: command_ping
+		handler: command_hlen
 	}
 
 	h << redisserver.RedisHandler{
 		command: 'HDEL'
-		handler: command_ping
+		handler: command_hdel
 	}
 
 
